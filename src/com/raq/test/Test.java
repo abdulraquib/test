c8d63fa164e804f6c04ec4aaa1fe020572141399
Test User <nooranargis@gmail.com>
2015-03-15 20:19:10 +0530

    User 2 local commit

--- src/com/raq/test/Test.java
+++ src/com/raq/test/Test.java
@@ -1,6 +1,11 @@
 package com.raq.test;
 
 public class Test {
+	
+	public static void testHello(){
+		System.out.println("testHello");		
+	}
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
