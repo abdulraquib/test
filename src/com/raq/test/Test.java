b54f5e9ee134afaa0ab30192d759c6692635b66b
Abdul Raquib <abdulraquib1@gmail.com>
2015-03-29 13:17:50 +0530

    commit 8

--- src/com/raq/test/Test.java
+++ src/com/raq/test/Test.java
@@ -3,17 +3,14 @@ package com.raq.test;
 public class Test {
 	
 	public static void testHello(){
-		System.out.println("testHello-commit7");		
+		System.out.println("testHello-commit8");		
 	}
 	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Test 1");
-		System.out.println("Test 3");
-		System.out.println("Test 4");
-		System.out.println("Test 5");
-		System.out.println("Test 7");
+		System.out.println("Test 8");
+		
 
 	}
 
