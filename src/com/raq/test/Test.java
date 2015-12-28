73e201f46d8946c6b48d239339afb46b2918c06b
abdulraquib <abdulraafay2006@gmail.com>
2015-12-28 22:56:22 +0530

    zxzx

--- src/com/raq/test/Test.java
+++ src/com/raq/test/Test.java
@@ -3,7 +3,10 @@ package com.raq.test;
 public class Test {
 	
 	public static int sum(int a, int b){
+<<<<<<< raquib-branch-1
 
+=======
+>>>>>>> 9c79c80 Commit : araquib SOP Sum
 		return a + b;
 		
 	}
