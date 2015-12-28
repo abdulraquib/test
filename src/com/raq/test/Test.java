0866f6477f9d15489c9391ed76a43436004091f7
abdulraquib <abdulraafay2006@gmail.com>
2015-12-28 22:44:56 +0530

    Raafay refined sum

--- src/com/raq/test/Test.java
+++ src/com/raq/test/Test.java
@@ -3,16 +3,15 @@ package com.raq.test;
 public class Test {
 	
 	public static int sum(int a, int b){
-		System.out.println("a + b" = a + b);
+
 		return a + b;
 		
 	}
-	//testing commit further -- raafay
 
-	//testing commit by raquib 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Test 8");
+		int a = 8;
+		int b = 9;
+		System.out.println("Sum of a : "+a +" : b "+ b +" :: "+  sum(a,b) );
 		
 	}
 
